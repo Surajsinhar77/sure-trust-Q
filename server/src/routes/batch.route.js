5e6a0a2e2c67dd8747218c97910e946c03a7d257
Suraj Kumar <surajsinhaking77@gmail.com>
2024-05-31 03:01:51 +0530

    done few left batch work

--- server/src/routes/batch.route.js
+++ server/src/routes/batch.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { addBatch, getBatch,getBatchById, updateBatch,deleteBatch } = require('../controller/batch.controller');
 
-router.route('/addBatch').post();
-router.route('/getBatch').get();
-router.route('/getBatchById/:id').get();
-router.route('/updateBatch/:id').put();
-router.route('/deleteBatch/:id').delete();
+router.route('/addBatch').post(addBatch);
+router.route('/getBatch').get(getBatch);
+router.route('/getBatchById/:id').get(getBatchById);
+router.route('/updateBatch/:id').put(updateBatch);
+router.route('/deleteBatch/:id').delete(deleteBatch);
 
 module.exports = router;
