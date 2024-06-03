058f388bd92ce7056b1e909a09af94a5cdd702e6
Suraj Kumar <surajsinhaking77@gmail.com>
2024-06-03 22:26:43 +0530

    done this chnage

--- server/src/middleware/auth.middleware.js
+++ server/src/middleware/auth.middleware.js
@@ -18,7 +18,7 @@ const verifyToken = (req, _, next) => {
             if(token) {
                 jwt.verify(token, secretkey, (err, decoded) => {
                     if (err) {
-
+                        console.log(err);
                         throw new ErrorHandling('Invalid token', 401);
                     }
                     req.user = decoded;
