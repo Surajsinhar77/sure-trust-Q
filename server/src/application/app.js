5fbc77a8152167a0a44391b645c8c54c2a46ac0a
Suraj Kumar <surajsinhaking77@gmail.com>
2024-06-10 01:15:53 +0530

    done this change

--- server/src/application/app.js
+++ server/src/application/app.js
@@ -16,7 +16,7 @@ app.use(cookiesParser());
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Replace with your frontend domain
+    origin: ['http://localhost:5173', 'https://suretrustqa.vercel.app'], // Replace with your frontend domain
     credentials: true // Allow credentials (cookies, authorization headers, etc.)
 };
 app.use(cors(corsOptions));
