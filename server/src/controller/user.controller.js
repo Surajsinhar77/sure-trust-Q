4e35e2dd3751b93007d393c21d185a98f7515038
Suraj Kumar <68435934+Surajsinhar77@users.noreply.github.com>
2024-06-10 01:55:15 +0530

    Update user.controller.js
    change a little on the getUser

--- server/src/controller/user.controller.js
+++ server/src/controller/user.controller.js
@@ -256,7 +256,7 @@ const refreshAccessToken = async (req, res) => {
 
 const getUser = async (req, res) => {
     try {
-        const userId = z.string().min(24).parse(req.params.id) || req.query.id;
+        const userId = z.string().min(24).parse(req?.params?.id || req?.query?.id); 
         const user = await userModel.findById(userId).select('-password -refreshToken');
         if (!user) {
             throw new ErrorResponse(404, 'User is not found');
