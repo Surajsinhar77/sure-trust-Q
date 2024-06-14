74be2c78d56d77da904236448654d059f939389f
Suraj Kumar <surajsinhaking77@gmail.com>
2024-06-15 03:34:40 +0530

    no awit in jwt.sign

--- server/src/service/genrateToken.service.js
+++ server/src/service/genrateToken.service.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
 
 const genrateAccessToken = async(payload) => {
-    return await jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' })
+    return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5m' })
 }
 
 const genrateRefreshToken = async(payload) =>{
-    return await jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn : '7d'});
+    return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET, {expiresIn : '7d'});
 }
 
 
